bae62410f5fc598a426f6e01de4c030715739e46
kkdYodoHonda <id0145@kokodeglobal.com>
2015-05-09 06:17:00 +0900

    yield specific period trimming and receiver list of that

--- MakeLog.scala
+++ MakeLog.scala
@@ -1,19 +1,29 @@
 import java.io.PrintWriter
 import scala.io.Source
 import java.util.Date
+import java.text.SimpleDateFormat
 
 object LogLines {
 	def processFile(filename: String, start: String, end: String) {
 		val date = "%tm%<td" format new Date
-		val st = start.take(2) + ":" + start.drop(2) + ":00"
-		val ed = end.take(2) + ":" + end.drop(2) + ":00"
+		val st = new SimpleDateFormat("HHmmss").parse(start + "00").getTime()
+		val ed = new SimpleDateFormat("HHmmss").parse(end + "00").getTime()
 
 		val source = Source.fromFile(filename)
 		val logname = "maillog_" + date + "_" + start + ".log"
 		val maillog = new PrintWriter(logname)
-		for (line <- source.getLines())
-			if (line matches """\s[0-9]{2}:[0-9]{2}:[0-9]\s""")
+		val regex = """.*\s(\d\d:\d\d:\d\d)\s.*""".r
+		def getLog(reg: String, line: String) {
+			var time = new SimpleDateFormat("HH:mm:ss").parse(reg).getTime()
+			if(st <= time && time < ed)
 				maillog.println(line)
+		}
+		for (line <- source.getLines()) {
+			line match {
+				case regex(reg) => getLog(reg, line)
+				case _ =>
+			}
+		}
 		maillog.close
 
 		val source2 = Source.fromFile(logname)
